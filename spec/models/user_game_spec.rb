04b4deab24fb07d5840d938588bf6b433da8a05b
Jonathan <jgg.9026@gmail.com>
2015-09-07 17:32:36 -0500

    se arreglo el error anterior, tuve que dropear la base de datos y migrarla nuevamente, ni idea cual era el error, ahora funciona la primera prueba de user_game asignando user y game

--- spec/models/user_game_spec.rb
+++ spec/models/user_game_spec.rb
@@ -11,12 +11,15 @@ RSpec.describe UserGame, type: :model do
   	it "user_game is ok"do
       user=FactoryGirl.create(:user, name: 'joj1')
       game=FactoryGirl.create(:game, name: 'joj2')
-      puts(user.name)
+      #puts(user.name)
   		usergame=FactoryGirl.build(:user_game, user: user, game: game)
-      puts(usergame.user_id)
-      puts(usergame.game_id)
-      puts("------------")
+      #puts(usergame.user_id)
+      #puts(usergame.game_id)
+      #puts(usergame.condition)
+      #puts("------------")
   		expect(usergame.save).to eq(true)
+      
+      
   	end
   	it "doesn't have a user id" do
   		usergame=FactoryGirl.build(:no_user_id)

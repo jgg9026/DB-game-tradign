0d3ff337d95212a0486ec277d076e5c6fd6631ae
Jonathan <jgg.9026@gmail.com>
2015-09-07 13:50:26 -0500

    se agregaron las pruebas a category, contenido y nombre unico

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ActiveRecord::Base
 	validates :name, presence: true
 	validates :name, uniqueness: true
 	has_many :categorizations
+	has_many :games, through: :categorizations
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context "testing category model" do
+		it "has a name" do
+			category=FactoryGirl.build(:category)
+			expect(category.save).to eq(true)
+		end	
+		it "has a unique name" do
+			category1=FactoryGirl.create(:category)
+			category2=FactoryGirl.build(:category)
+			expect(category2.save).to eq(false)
+		end
+	end
 end

4de71dbe4ce41751f3d3e2e64f4143657a312929
Jonathan <jgg.9026@gmail.com>
2015-09-07 13:21:07 -0500

    se agregaron pruebas a exchange_record, utilizando factories existentes

--- spec/factories/exchange_records.rb
+++ spec/factories/exchange_records.rb
@@ -4,8 +4,9 @@ FactoryGirl.define do
 	exchange_id 1
 	user_id 1
 
-	
+	factory :no_user_game_id do
+		user_game_id nil
+	end
+
   end
-
-
 end
--- spec/models/exchange_record_spec.rb
+++ spec/models/exchange_record_spec.rb
@@ -6,10 +6,20 @@ RSpec.describe ExchangeRecord, type: :model do
   		exchangerecord=FactoryGirl.build(:exchange_record)
   		expect(exchangerecord.save).to eq(true)
   	end
-  	it "doesn't have user_game_id" do
+  	it "doesn't have exchange_game_id" do
   		exchangerecord=FactoryGirl.build(:no_exchange_id)
   		expect(exchangerecord.save).to eq(false)
 
+  	end
+
+  	it "doesn't have user_game_id" do
+  		exchangerecord=FactoryGirl.build(:no_user_game_id)
+  		expect(exchangerecord.save).to eq(false)
+  	end
+
+  	it "doesn't have user_id" do
+  		exchangerecord=FactoryGirl.build(:no_user_id)
+  		expect(exchangerecord.save).to eq(false)
   	end
 
   end
